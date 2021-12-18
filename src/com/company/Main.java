c91acefabcfacc900cf46b1bedf393c14c548e08
edwedja <edwedja_lima@hotmail.com>
2021-12-17 22:54:08 -0300

    JogoDaVelha

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,112 +1,158 @@
 package com.company;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner (System.in); //instanciando a classe
+        String estadoJogo = "INICIO";
+        int[] posicoesEscolhidas = new int [9];
+        Random random = new Random();
+        int rand;
+        System.out.println("Bem vindo ao Jogo da Velha, você deverá fazer uma sequência de 3 'X' na vertical," +
+                "horizontal ou diagonal, para ganhar o jogo, segue a baixo a tabela com as possíveis posições: ");
+        char[][] exemp = {
+                {'1', '|','2', '|', '3'},
+                {'4', '|','5', '|', '6'},
+                {'7', '|','8', '|', '9'}
+        };
+        printTabuleiro(exemp);
 
-        Scanner scan = new Scanner (System.in);
+        System.out.println("Faça sua escolha e comece:");
 
-        // AULA 4 -  DE CONTROLE DE FLUXOS (IF, ELSE, ETC...)
-        /*
-         * pessoas < 16 -> não podem votar
-         * pessoas >= 16 -> opcional && < 18
-         * pessoas > 18 -> obrigatório
-         * pessoas < 70 -> obrigatório
-         * pessoas > 70 -> opcional*/
+        char[][] tabuleiro = {
+                {'_', '|','_', '|', '_'},
+                {'_', '|','_', '|', '_'},
+                {' ', '|',' ', '|', ' '}
+        };
+        printTabuleiro(tabuleiro);
 
-        System.out.print("Informe a sua idade: ");
-        //int ag = Integer.parseInt(scan.nextLine());
-        int ag = scan.nextInt();
+        // Solicitando as jogadas:
+        int i = 0;
+        int in;
+        while (i<9){
 
-        if(ag >= 16 &&  ag < 18 || ag > 70){
-            System.out.println("Seu voto é opcional");
-        } else if (ag < 16){
-            System.out.println("Você não pode votar");
+            if (i%2 == 0) { //vez do player
+                System.out.printf("Digite a %d ª posição escolhida: ", i+1);
+                in = scan.nextInt();
+
+                //verificando se a posição já foi escolhida:
+                if (posicoesEscolhidas[in-1]==1 || in >9 || in <=0){
+                    System.out.println("Esta posição já foi escolhida anteriormente, ou não é valida, favor escolha outra!! ");
+                    i--;
+                }else{
+                    posicoesEscolhidas[in-1] = 1;
+                    atualizaTabuleiro(in,2,tabuleiro);
+                }
+
+
+            }else{ //vez do bot
+
+                in= random.nextInt(8)+1;
+                //verificando se a posição já foi escolhida:
+                if (posicoesEscolhidas[in-1]==1 || in >9 || in <=0){
+                    i--;
+                }else{
+                    System.out.println("Jogada do PC!! ");
+                    posicoesEscolhidas[in-1] = 1;
+                    atualizaTabuleiro(in,1,tabuleiro);
+                }
+
+            }
+
+            //Verificando se há vencedor:
+            for (int j=0; j<3; j++){// verificado as linhas
+                if(tabuleiro[j][0] == 'X'&&  tabuleiro[j][2] == 'X'&& tabuleiro[j][4] == 'X'){
+                    System.out.println("QUE PENA!! VOCÊ PERDEU O JOGO!  ");
+                    i=10;
+                }else  if(tabuleiro[j][0] == 'O'&&  tabuleiro[j][2] == 'O'&& tabuleiro[j][4] == 'O'){
+                    System.out.println("PARABÉNS!! VOCÊ GANHOU O JOGO!  ");
+                    i=10;
+                }
+            }
+
+            for (int j=0; j<5; j=j+2){ // verificado as colunas
+                if(tabuleiro[0][j] == 'X'&&  tabuleiro[1][j] == 'X'&& tabuleiro[2][j] == 'X'){
+                    System.out.println("QUE PENA!! VOCÊ PERDEU O JOGO!  ");
+                    i=10;
+                }else  if(tabuleiro[0][j] == 'O'&&  tabuleiro[1][j] == 'O'&& tabuleiro[2][j] == 'O'){
+                    System.out.println("PARABÉNS!! VOCÊ GANHOU O JOGO!  ");
+                    i=10;
+                }
+            }
+
+            //verificando diagonais:
+            if((tabuleiro[0][0] == 'X' && tabuleiro[1][2] == 'X' &&tabuleiro[2][4] == 'X') || (tabuleiro[2][0] == 'X' && tabuleiro[1][2] == 'X' &&tabuleiro[0][4] == 'X') ){
+                System.out.println("QUE PENA!! VOCÊ PERDEU O JOGO!  ");
+                i=10;
+            }else if((tabuleiro[0][0] == 'O' && tabuleiro[1][2] == 'O' &&tabuleiro[2][4] == 'O') || (tabuleiro[2][0] == 'O' && tabuleiro[1][2] == 'O' &&tabuleiro[0][4] == 'O') ){
+                System.out.println("PARABÉNS!! VOCÊ GANHOU O JOGO!  ");
+                i=10;
+            }
+
+
+            i++;
+        }
+    }
+
+    public static void printTabuleiro (char[][] tabuleiro){ // método
+        for (char[] row: tabuleiro){ // forEach
+            for(char colun: row){
+                System.out.print(colun);
+            }
+            System.out.println();
+        }
+    }
+
+    // JOGADOR 1 = 'x' -> Bot
+    // JOGADOR 2 = 'o' -> player
+    public static void atualizaTabuleiro(int position, int player, char[][] tabuleiro) {
+        char character;
+
+        if(player == 1){
+            character = 'X';
         }else {
-            System.out.println("Seu voto é obrigatório");
+            character = 'O';
         }
 
-        int month = 1;
-        String monthString;
-
-        switch (month) {
-            case 1:
-                monthString = "Janeiro";
+        switch (position){
+            case 1: tabuleiro[0][0] =character;
+                printTabuleiro(tabuleiro);
                 break;
-            case 2:
-                monthString = "Fevereiro";
+            case 2: tabuleiro[0][2] =character;
+                printTabuleiro(tabuleiro);
                 break;
-            case 3:
-                monthString = "Março";
+            case 3: tabuleiro[0][4] =character;
+                printTabuleiro(tabuleiro);
+                break;
+            case 4: tabuleiro[1][0] =character;
+                printTabuleiro(tabuleiro);
+                break;
+            case 5: tabuleiro[1][2] =character;
+                printTabuleiro(tabuleiro);
+                break;
+            case 6: tabuleiro[1][4] =character;
+                printTabuleiro(tabuleiro);
+                break;
+            case 7: tabuleiro[2][0] =character;
+                printTabuleiro(tabuleiro);
+                break;
+            case 8: tabuleiro[2][2] =character;
+                printTabuleiro(tabuleiro);
+                break;
+            case 9: tabuleiro[2][4] =character;
+                printTabuleiro(tabuleiro);
                 break;
             default:
-                monthString = "teste";
-        }
-
-        System.out.println(monthString);
-
-        int mes;
-        int dias = 31;
-
-        System.out.println("Digite o mês [1-12]: ");
-        mes = scan.nextInt();
-
-        if (mes > 12 || mes < 1) {
-            System.out.println("Mês inválido");
-        }
-
-        switch (mes){
-            case 2:
-                dias = dias -3;
                 break;
-            case 4 :
-            case 6:
-            case 9:
-            case 11:
-                dias = dias -1;
-                break;
-
         }
-        System.out.printf("O mês %d possui %d dias", mes, dias);
-
-
-        // Estruturas de repetição!  While/Do-while/ for
-
-        int count = 0;
-//        while (count < 11){
-//            System.out.println("Count is: " + count);
-//            count +=1;
-//        }
-
-        do{
-            System.out.println("Count is: " + count);
-            count +=1;
-        } while (count < 11);
-
-        // MÉDIA DE NOTAS
-
-        float notas = 0f;
-        float total = 0f;
-        float media = 0f;
-        int cont = 0;
-
-        while (cont<3){
-            cont +=1;
-            System.out.printf("Informe a %d nota", cont);
-            notas = scan.nextFloat();
-            total += notas;
-        }
-        media = total /3;
-        System.out.printf(" A média é %f", media);
-
-
-        for (int i =0; i< 11; i++){
-            System.out.println(i);
-        }
-
-
     }
+
+
+
+
+
 }
